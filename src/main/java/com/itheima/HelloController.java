e66165aac71d9c3b523b793292b55209fb9bbc13
laj <483806716@qq.com>
2023-04-03 14:58:44 +0800

    获取配置文件中自定义的三种方式

--- src/main/java/com/itheima/HelloController.java
+++ src/main/java/com/itheima/HelloController.java
@@ -1,10 +1,49 @@
 package com.itheima;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+//通个@Value来获取配置文件中的属性
+    //    @Value("${name}")
+    @Value("${person.name}")
+    private String name1;
+
+    @Value("${address[0]}")
+    private String address;
+
+    @Value("${msg1}")
+    private String msg1;
+
+    @Value("${msg2}")
+    private String msg2;
+    //通个Environment对象来获取配置文件中的属性
+    @Autowired
+    private Environment environment;
+    @Autowired
+    private Person person;
+
+    /**
+     * 测试从配置文件中读取到的值
+     * @return
+     */
+    @RequestMapping("/getArg")
+    public String getArg (){
+        System.out.println(name1);
+        System.out.println(address);
+        System.out.println(msg1);
+        System.out.println(msg2);
+        System.out.println("-----------");
+        System.out.println(environment.getProperty("name"));
+        System.out.println("-----------");
+        System.out.println(person.toString());
+        return  "";
+    }
+
 
     @RequestMapping("/hello")
     public String hello (){
---
+++ src/main/java/com/itheima/Person.java
@@ -0,0 +1,36 @@
+package com.itheima;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+//使用@ConfigurationProperties将配置文件中的对象数据域对象类的属性绑定
+@Component
+@ConfigurationProperties(prefix = "person2")
+public class Person {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
